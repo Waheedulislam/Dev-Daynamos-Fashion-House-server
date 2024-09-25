e4ecf8a8b21db5a4a6b91e68aec4afb888053d86
Whaeedil <baoppyhossen1234@gmail.com>
2024-09-26 00:07:10 +0600

    product-get

--- index.js
+++ index.js
@@ -23,6 +23,46 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    // Users
+    const usersCollection = client.db("UsersDB").collection("Users");
+    const productCollection = client.db("productDB").collection("Products");
+
+    ////////////////////// User Collection ////////////////////////
+    // Users Post
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({
+          massage: "User Already Exists",
+          insertedIn: null,
+        });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // Users GET
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+
+      res.send(result);
+    });
+
+    ////////////////////// Product Collection ////////////////////////
+
+    // post all product
+    app.post("/products/add", async (req, res) => {
+      const products = req.body;
+      const result = await productCollection.insertOne(products);
+      res.send(result);
+    });
+    // get all products
+    app.get("/products/all", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
     console.log("You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
