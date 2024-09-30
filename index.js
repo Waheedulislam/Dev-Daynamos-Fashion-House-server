905b134bd37c9e1498420d1f33f1dcd6bd024333
Whaeedil <baoppyhossen1234@gmail.com>
2024-09-30 21:26:18 +0600

    adminn-jwt

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cn4db.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,12 +26,56 @@ async function run() {
     // Users
     const usersCollection = client.db("UsersDB").collection("Users");
     const productCollection = client.db("productDB").collection("Products");
+    const blogsCollection = client.db("BlogsDB").collection("Blogs");
+    const wishlistCollection = client.db("WishlistDB").collection("Wishlist");
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      // console.log(token);
+      res.send({ token });
+    });
+    // jwt middleware
+    const verifyToken = (req, res, next) => {
+      // console.log("inside-Token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ massage: "Unauthorize access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        function (err, decoded) {
+          if (err) {
+            return res.status(401).send({ massage: "Unauthorize access" });
+          }
+          req.decoded = decoded;
+          next();
+        }
+      );
+    };
+    // use verify admin after
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
 
     ////////////////////// User Collection ////////////////////////
     // Users Post
     app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user?.email };
+      const query = {
+        email: user?.email,
+      };
       const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
         return res.send({
@@ -43,12 +87,56 @@ async function run() {
       res.send(result);
     });
     // Users GET
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
 
       res.send(result);
     });
 
+    // admin check
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req?.params?.email;
+      if (email !== req?.decoded?.email) {
+        return res.status(403).send({ massage: "forbidden access" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+    // Users patch make admin
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req?.params?.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
+    // Users Delete
+    app.delete(
+      "/users/delete/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req?.params?.id;
+        const result = await usersCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+        res.send(result);
+      }
+    );
     ////////////////////// Product Collection ////////////////////////
 
     // post all product
@@ -58,9 +146,198 @@ async function run() {
       res.send(result);
     });
     // get all products
+    // app.get("/products/all", async (req, res) => {
+    //   const result = await productCollection.find().toArray();
+    //   res.send(result);
+    // });
+
+    // all product get
     app.get("/products/all", async (req, res) => {
-      const result = await productCollection.find().toArray();
+      try {
+        const {
+          brands,
+          ram,
+          colors,
+          driveSizes,
+          gpuBrands,
+          processors,
+          screenSizes,
+        } = req.query;
+
+        // Initialize query object
+        const query = {};
+
+        // Add filters to the query object if they exist
+        if (brands) query.brand = { $in: brands.split(",") };
+        if (ram) query.ram = { $in: ram.split(",") };
+        if (colors) query.color = { $in: colors.split(",") };
+        if (driveSizes) query.driveSize = { $in: driveSizes.split(",") };
+        if (gpuBrands) query.gpuBrand = { $in: gpuBrands.split(",") };
+        if (processors) query.processor = { $in: processors.split(",") };
+        if (screenSizes) query.screenSize = { $in: screenSizes.split(",") };
+
+        // Fetch products based on filters or all products if no filters are provided
+        const productsCursor =
+          Object.keys(query).length === 0
+            ? productCollection.find()
+            : productCollection.find(query);
+
+        // Convert cursor to an array
+        const products = await productsCursor.toArray();
+
+        // Send back the products or an empty array if no products exist
+        res.status(200).json(products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ message: "Error fetching products" });
+      }
+    });
+
+    // get single products
+    app.get("/products/details/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const result = await productCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
+    });
+    // patch product
+    app.patch("/products/edit/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const editProduct = req.body;
+      const result = await productCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          $set: editProduct,
+        }
+      );
+      res.send(result);
+    });
+    // delete product
+    app.delete("/products/delete/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const result = await productCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
+    ////////////////////// Blog Collection ////////////////////////
+
+    // Blogs add
+    app.post("/blogs/add", async (req, res) => {
+      const blogs = req.body;
+      const result = await blogsCollection.insertOne(blogs);
+      res.send(result);
+    });
+
+    // Blogs get
+    app.get("/blogs/list", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+    // Blogs single data get
+    app.get("/blogs/details/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const result = await blogsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+    // Blogs Delete
+    app.delete("/blogs/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await blogsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    ////////////////////// wishlist Collection ////////////////////////
+
+    // wishlist post
+    app.post("/wishlist/add", async (req, res) => {
+      const { user, product } = req.body;
+
+      try {
+        // Check if the wishlist for the user already exists
+        let wishlist = await wishlistCollection.findOne({ userId: user });
+
+        if (!wishlist) {
+          // If no wishlist exists, create a new one
+          wishlist = {
+            userId: user,
+            products: [product], // Assuming product._id is the product ID
+          };
+          await wishlistCollection.insertOne(wishlist);
+        } else {
+          // If wishlist exists, check if the product is already in the wishlist
+          if (!wishlist.products.includes(product)) {
+            // Add the product ID to the wishlist
+            wishlist.products.push(product);
+            await wishlistCollection.updateOne(
+              { userId: user },
+              { $set: { products: wishlist.products } }
+            );
+          }
+        }
+
+        res
+          .status(200)
+          .json({ message: "Product added to wishlist", wishlist });
+      } catch (error) {
+        console.error("Error adding product to wishlist:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    // wishlist get
+    // GET API to retrieve wishlist for a specific user
+    app.get("/wishlist/:userId", async (req, res) => {
+      const { userId } = req.params; // Extract userId from request parameters
+
+      try {
+        // Find the wishlist for the given userId
+        const wishlist = await wishlistCollection.findOne({ userId: userId });
+
+        if (!wishlist) {
+          return res.status(404).json({ message: "Wishlist not found" });
+        }
+
+        // Return the wishlist with the products
+        res.status(200).json(wishlist);
+      } catch (error) {
+        console.error("Error retrieving wishlist:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    // DELETE API to remove a product from the wishlist
+    app.delete("/wishlist/remove", async (req, res) => {
+      const { userId, productId } = req.body; // Receiving userId and productId from the request body
+
+      try {
+        // Find the user's wishlist
+        const wishlist = await wishlistCollection.findOne({ userId });
+
+        if (!wishlist) {
+          return res.status(404).json({ message: "Wishlist not found" });
+        }
+
+        // Filter out the product from the products array
+        const updatedProducts = wishlist.products.filter(
+          (product) => product._id !== productId
+        );
+
+        // Update the wishlist with the new products array
+        await wishlistCollection.updateOne(
+          { userId },
+          { $set: { products: updatedProducts } }
+        );
+
+        res
+          .status(200)
+          .json({ message: "Product removed from wishlist", updatedProducts });
+      } catch (error) {
+        console.error("Error removing product from wishlist:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
     });
 
     console.log("You successfully connected to MongoDB!");
