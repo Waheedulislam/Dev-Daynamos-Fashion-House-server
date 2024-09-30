b7d354ad2652a9ca80d6aec0ab724e4cc8be6f43
freelancersifat627e <freelancersifat627@gmail.comm>
2024-09-30 18:19:50 +0600

    create payment post api & aslo save user payment info in database

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ require("dotenv").config();
 const app = express();
 const jwt = require("jsonwebtoken");
 const port = 5000;
+const { v4: uuidv4 } = require('uuid'); // Import UUID for unique transaction IDs
+
+
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { default: axios } = require("axios");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cn4db.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +33,7 @@ async function run() {
     const productCollection = client.db("productDB").collection("Products");
     const blogsCollection = client.db("BlogsDB").collection("Blogs");
     const wishlistCollection = client.db("WishlistDB").collection("Wishlist");
+    const paymentCollection = client.db("paymentDB").collection("payment");
 
     ////////////////////// User Collection ////////////////////////
     // Users Post
@@ -259,14 +265,84 @@ async function run() {
 
     ////////////////////////////////////// Payment Collection Start ///////////////////////////
 
-// payment post api 
+// payment post api:
 app.post('/create-payment', async(req,res) => {
   const paymentInfo = req.body;
-  console.log(paymentInfo)
-  res.send('result')
+  console.log(paymentInfo);
+
+  const tran_id = uuidv4();
+
+  const initiateData = {
+  store_id: process.env.SSL_STORE_ID,
+  store_passwd: process.env.SSL_STORE_PASSWORD,
+  total_amount: paymentInfo.amount,
+  currency: "EUR",
+  tran_id: tran_id,
+  success_url: "http://localhost:5000/success-payment",
+  fail_url: "http://yoursite.com/fail.php",
+  cancel_url: "http://yoursite.com/cancel.php",
+  cus_name: "Customer Name",
+  cus_email: "cust@yahoo.com",
+  cus_add1: "Dhaka",
+  cus_add2: "Dhaka",
+  cus_city: "Dhaka",
+  cus_state: "Dhaka",
+  cus_postcode: "1000",
+  cus_country: "Bangladesh",
+  cus_phone: "01711111111",
+  cus_fax: "01711111111",
+  shipping_method: "NO",
+  product_name: "Computer",
+  product_category: "Electronic",
+  product_profile: "general",	
+  multi_card_name: "mastercard,visacard,amexcard",
+  value_a: "ref001_A",
+  value_b: "ref002_B",
+  value_c: "ref003_C",
+  value_d: "ref004_D"
+  };
+  
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: "https://sandbox.sslcommerz.com/gwprocess/v4/api.php",
+      data: initiateData,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded',},
+    });
+
+    console.log('response data 313:' , response.data);
+
+    // Send Data to The Database
+    const saveData = {
+      userName: 'user name',
+      paymentId: tran_id,
+      amount: paymentInfo.amount,
+      status: 'Pending',
+    }
+
+    const saveUserInfoInDb = await paymentCollection.insertOne(saveData);
+
+    if(saveUserInfoInDb){
+      res.send({
+        paymentUrl: response.data.GatewayPageURL,
+      })
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+
+  // console.log(response);
+
+  // res.json(response.data);
 });
 
-
+// success payment api:
+app.post('/success-payment', async (req, res) => {
+  const successData = req.body;
+  console.log('success data 307:' , successData);
+  res.json({ message: 'Payment successful', data: successData });
+});
 
 
 
