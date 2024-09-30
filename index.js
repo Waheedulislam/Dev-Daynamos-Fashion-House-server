ced900d23f0c36cb06fb7b783eb2df32f3801314
Whaeedil <baoppyhossen1234@gmail.com>
2024-09-30 15:47:48 +0600

    make-admin

--- index.js
+++ index.js
@@ -52,6 +52,24 @@ async function run() {
 
       res.send(result);
     });
+    // Users Delete
+    app.delete("/users/delete/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+    // Users patch make admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     ////////////////////// Product Collection ////////////////////////
 
